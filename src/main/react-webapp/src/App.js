1b0d0c2f1e99346f485c69ec5e7e2a3f78fa32b3
Edith Benvenuto <edithpau_benvenuto@hotmail.com>
2021-07-18 02:38:27 +0000

    Add all drugs page

--- src/main/react-webapp/src/App.js
+++ src/main/react-webapp/src/App.js
@@ -2,6 +2,8 @@ import './css/App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from "./components/Home";
 import Example from "./components/Example"
+import Navbar from './components/Navbar';
+import AllDrugs from './components/AllDrugs';
 
 function App() {
 
@@ -10,6 +12,9 @@ function App() {
         <Route exact path="/" component={Home}/>
           {/*this is just an example of how a route should look like work and should be removed*/}
         <Route exact path="/example" component={Example}/>
+        <Route path="/All-Drugs">
+            <AllDrugs/>
+        </Route>
       </Router>
   );
 }
