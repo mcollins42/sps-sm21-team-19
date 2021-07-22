63ceac4c71aa42a6373930b400f6b405f6d44e7a
Garrett Marsh <gmarsh0218@gmail.com>
2021-07-21 21:25:56 -0400

    Add api call to get page content

--- src/main/react-webapp/src/components/DrugPage.js
+++ src/main/react-webapp/src/components/DrugPage.js
@@ -1,41 +1,33 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from "react";
 import { useParams } from 'react-router-dom';
-import { fetchJson } from '../js/fetchJson'
 import DrugPageList from "./DrugPageList";
+import DrugPageDesc from "./DrugPageDesc";
+import '../css/DrugPage.css';
 
 const DrugPage = () => {
-
+ let [drugInfo, setDrugInfo] = useState({});
  let {id} = useParams();
 
- // const [drugInfo, setDrugInfo] = useEffect(
- //     await fetchJson(`/list-drug-info?id=${id}`)
- //     , []);
+ useEffect(() => {
+  getDrugInfo(id)
+  }, []);
 
- const drugInfo = {"id":1,"name":"Methadone",
-  "classification":"Opioid",
-  "aliases":["Fizzies","Amidone","Chocolate Chip Cookies"],
-  "safetyDescription":"Methadone is safe and effective, when taken as prescribed. Methadone medication is specifically tailored for the individual patient (and doses are often adjusted and readjusted) and is never to be shared with or given to others. ",
-  "drugWarningSigns":["sdasd","asd","sample","asdasdas"],
-  "description":"sample desc",
-  "effects":["Low heart rate","Slow breathing rate","Cooler skin temperature","Constricted pupils","Relaxed muscles","Sweating","Sleepy","Respiratory or Cardiac arrest."],
-  "overdoseSigns":["sample","Sample","sample","Constricted pupils"],
-  "legalLocations":["sda","Sample","sample","Constricted pupils"],
-  "decriminalizedLocations":["sample","sadasd","sample","asdasda"]
- };
-
-
-
+ const getDrugInfo = async (id) => {
+  const res = await fetch(`/list-drug-info?name=${id}`);
+  const jsonResponse = await res.json()
+  setDrugInfo(jsonResponse);
+ }
 
  return (
-     <div className="drug-page">
-      <h1>{drugInfo["name"]}</h1>
-      <DrugPageList title="Aliases:" listItems={drugInfo["aliases"]}/>
-      <p>Classification: {drugInfo["classification"]}</p>
-      {drugInfo["safetyDescription"] ? <p><strong>Safety Description</strong> {drugInfo["safetyDescription"]}</p> : null}
-      <DrugPageList title="Signs of Overdose:" listItems={drugInfo["overdoseSigns"]}/>
-
-     </div>
- )
-}
+       <div className="drug-page">
+        <h1>{drugInfo["name"]}</h1>
+        <p><b>Classification:</b> {drugInfo["classification"]}</p>
+        <DrugPageDesc title="Description" desc={drugInfo["description"]}/>
+        <DrugPageList title="Aliases" listItems={drugInfo["aliases"]}/>
+        <DrugPageList title="Effects" listItems={drugInfo["effects"]}/>
+        <DrugPageList title="Signs of Overdose" listItems={drugInfo["overdoseSigns"]}/>
+       </div>
+   )
+};
 
 export default DrugPage;
