b35e07c7dcfa1fc46bf1486a3678ed584f5160c7
Garrett Marsh <gmarsh0218@gmail.com>
2021-07-25 16:58:19 -0400

    Create searchbar component

---
+++ src/main/react-webapp/src/components/Searchbar.js
@@ -0,0 +1,47 @@
+import {ReactSearchAutocomplete} from 'react-search-autocomplete';
+import {useHistory} from 'react-router-dom';
+import {useEffect, useState} from "react";
+
+const Searchbar = () => {
+
+ const [drugs, setDrugs] = useState([])
+
+ useEffect(() => {
+  const getData = async () => {
+	const data = await fetch('/list-drugs')
+	const allDrugs = await data.json()
+	setDrugs(allDrugs)
+  }
+  getData()
+ }, [])
+
+ let history = useHistory();
+
+ const handleOnSelect = (item) => {
+  // redirect the user to the selected site
+  history.push(`/drug/${item.id}`);
+ }
+
+ const formatResult = (item) => {
+  return item;
+ }
+
+
+ let items = drugs.map(drug => items.push({id: drug["id"], name: drug["name"], aliases: drug["aliases"].join(' ')}))
+
+
+ return (
+	  <div className="search-bar-wrapper">
+		<ReactSearchAutocomplete
+			 items={items}
+			 fuseOptions={{keys: ["name", "aliases"]}}
+			 resultStringKeyName="name"
+			 onSelect={handleOnSelect}
+			 autoFocus
+			 formatResult={formatResult}
+		/>
+	  </div>
+ )
+}
+
+export default Searchbar;
